5ee5039114ec8d38d95525e42a27fe235e5ed8b9
jpaarhuis <jpaarhuis@home.nl>
2019-04-22 20:23:41 +0200

    Update giant_check_sync.sh

--- Giant/giant_check_sync.sh
+++ Giant/giant_check_sync.sh
@@ -40,7 +40,7 @@ for FILE in ~/bin/giantd_$PARAM1.sh; do
 	  WALLETVERSION=$(echo $WALLETVERSION | tr 'version : ' " ")
 	  WALLETVERSION=$(echo $WALLETVERSION | tr -d ' ' )
 	  
-	  if ! [ "$WALLETVERSION" == "1020201" ]; then
+	  if ! [ "$WALLETVERSION" == "1030000" ]; then
 	     echo "!!!Your wallet $GIANTNAME is OUTDATED!!!"
 	  fi
 
